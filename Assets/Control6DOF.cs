890207c3e2f9910721b45162e129a021680c7978
kaustubnvd <kaustub.nvd@gmail.com>
2021-09-18 11:56:16 -0500

    Initial Commit

---
+++ Assets/Control6DOF.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class Control6DOF : MonoBehaviour
+{
+
+    private MLInput.Controller _controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = MLInput.GetController(MLInput.Hand.Left);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _controller.Position;
+        transform.rotation = _controller.Orientation;
+    }
+}
