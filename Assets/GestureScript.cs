00d8b91a5fd045c61c3f0a06135034b62e7c3123
kaustubnvd <kaustub.nvd@gmail.com>
2021-09-18 23:16:59 -0500

    Basic Gesture-Actions added

--- Assets/GestureScript.cs
+++ Assets/GestureScript.cs
@@ -5,13 +5,18 @@ using UnityEngine.XR.MagicLeap;
 
 public class GestureScript : MonoBehaviour
 {
-  // Start is called before the first frame update
   private MLHandTracking.HandKeyPose[] gestures; // Holds the different hand poses we will look for
   private GameObject cube; // Reference to our Cube
   private Renderer cubeRender;
   private bool openFist;
+  public Vector3[] pos;
+  public GameObject objectToPlace;
+  public GameObject selectedGameObject;
+  bool canIPlace = false;
+  bool canIGrab = false;
+
   void Start()
-    {
+  {
     MLHandTracking.Start(); // Start the hand tracking.
 
     gestures = new MLHandTracking.HandKeyPose[4]; //Assign the gestures we will look for.
@@ -25,28 +30,82 @@ public class GestureScript : MonoBehaviour
     cube = GameObject.Find("Cube"); // Find our Cube in the scene.
 
     cubeRender = cube.GetComponent<Renderer>();
+    cube.SetActive(false);
+
+    pos = new Vector3[3];
+    InitPoints();
   }
 
-    // Update is called once per frame
-    void Update()
+  private void InitPoints()
+  {
+    pos[0] = MLHandTracking.Left.Thumb.KeyPoints[2].Position;
+    pos[1] = MLHandTracking.Left.Index.KeyPoints[2].Position;
+    pos[2] = MLHandTracking.Left.Wrist.KeyPoints[0].Position;
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if(!canIGrab) 
     {
-    if ((GetGesture(MLHandTracking.Left, MLHandTracking.HandKeyPose.Fist)
-       || GetGesture(MLHandTracking.Right, MLHandTracking.HandKeyPose.Fist)) && openFist) {
-           openFist = false;
-            cubeRender.material.SetColor("_Color", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f));
-        } else if (!(GetGesture(MLHandTracking.Left, MLHandTracking.HandKeyPose.Fist)
-       || GetGesture(MLHandTracking.Right, MLHandTracking.HandKeyPose.Fist))){
-            openFist = true;
-        }
+      selectedGameObject = this.gameObject;
+      canIGrab = true;
+      this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+        
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+      canIGrab = false;
+      this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+
+  }
+  // Update is called once per frame
+  const bool HIGHER_THRESHOLD = true;
+  void Update()
+  {
+    //Debug.Log("Update");
+    //Press Button
+    //transform.position = pos[0];
+    if (GestureMade(MLHandTracking.HandKeyPose.Fist, HIGHER_THRESHOLD) && openFist)
+    {
+      openFist = false;
+      cubeRender.material.SetColor("_Color", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f));
+    }
+    else if (!GestureMade(MLHandTracking.HandKeyPose.Fist, !HIGHER_THRESHOLD))
+    {
+      openFist = true;
+      cube.SetActive(true);
     }
 
-  bool GetGesture(MLHandTracking.Hand hand, MLHandTracking.HandKeyPose type)
+    if (GestureMade(MLHandTracking.HandKeyPose.Pinch, HIGHER_THRESHOLD))
+    {
+      if (canIGrab)
+      {
+        selectedGameObject.transform.position = transform.position;
+        gameObject.GetComponent<SphereCollider>().radius = 1f;
+      }
+    }
+    else if (GestureMade(MLHandTracking.HandKeyPose.Pinch, !HIGHER_THRESHOLD))
+    {
+      GameObject placedObject = Instantiate(objectToPlace, pos[0], transform.rotation);
+      canIPlace = false;
+    }
+  }
+
+  bool GestureMade(MLHandTracking.HandKeyPose type, bool useHigherThresh)
+  {
+    float thresh = useHigherThresh ? 0.9f : 0.1f;
+    return GetGesture(MLHandTracking.Left, type, thresh) || GetGesture(MLHandTracking.Right, type, thresh);
+  }
+
+  bool GetGesture(MLHandTracking.Hand hand, MLHandTracking.HandKeyPose type, float thresh)
   {
     if (hand != null)
     {
       if (hand.KeyPose == type)
       {
-        if (hand.HandKeyPoseConfidence > 0.9f)
+        if (hand.HandKeyPoseConfidence > thresh)
         {
           return true;
         }
--- Assets/HandTrackingScript.cs
+++ Assets/HandTrackingScript.cs
@@ -3,35 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.MagicLeap;
 // Code adapted from Magic Leap Docs: 
-public class HandScript : MonoBehaviour
+public class HandTrackingScript : MonoBehaviour
 {
-    public enum HandPoses { Ok, Finger, Thumb, OpenHand, Fist, NoPose };
+    public enum HandPoses { Ok, Finger, Thumb, OpenHand, Fist, Pinch, NoPose };
     public HandPoses pose = HandPoses.NoPose;
     public Vector3[] pos;
     public GameObject sphereThumb, sphereIndex, sphereWrist;
+    public GameObject ObjectToPlace;
+    public GameObject selectedGameObject;
+    bool canPlace;
+    bool canGrab;
 
     private MLHandTracking.HandKeyPose[] _gestures;
 
     private void Start()
     {
         MLHandTracking.Start();
-        _gestures = new MLHandTracking.HandKeyPose[5];
+        _gestures = new MLHandTracking.HandKeyPose[6];
         _gestures[0] = MLHandTracking.HandKeyPose.Ok;
         _gestures[1] = MLHandTracking.HandKeyPose.Finger;
         _gestures[2] = MLHandTracking.HandKeyPose.OpenHand;
         _gestures[3] = MLHandTracking.HandKeyPose.Fist;
         _gestures[4] = MLHandTracking.HandKeyPose.Thumb;
+        _gestures[5] = MLHandTracking.HandKeyPose.Pinch;
         MLHandTracking.KeyPoseManager.EnableKeyPoses(_gestures, true, false);
         pos = new Vector3[3];
+        canPlace = true;
+        canGrab = false;
     }
     private void OnDestroy()
     {
         MLHandTracking.Stop();
     }
 
+    public void OnTriggerEnter(Collider other) {
+        if(!canGrab) {
+            selectedGameObject = other.gameObject;
+            canGrab = true;
+            other.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        canGrab = false;
+        other.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+
 
     private void Update()
     {
+        ShowPoints();
+        transform.position = pos[1];
         if (GetGesture(MLHandTracking.Left, MLHandTracking.HandKeyPose.Ok))
         {
             pose = HandPoses.Ok;
@@ -51,13 +73,27 @@ public class HandScript : MonoBehaviour
         else if (GetGesture(MLHandTracking.Left, MLHandTracking.HandKeyPose.Thumb))
         {
             pose = HandPoses.Thumb;
+            if(canPlace) {
+                GameObject objPlaced = Instantiate(ObjectToPlace, pos[0], transform.rotation);
+                canPlace = false;
+            }
         }
-        else
+        else if (GetGesture(MLHandTracking.Left, MLHandTracking.HandKeyPose.Pinch)) {
+            pose = HandPoses.Pinch;
+            if(canGrab) {
+                selectedGameObject.transform.position = transform.position;
+                gameObject.GetComponent<SphereCollider>().radius = 0.1f;
+            }
+        } else
         {
             pose = HandPoses.NoPose;
+            canPlace = true;
+            if(selectedGameObject && !canGrab) {
+                selectedGameObject = null;
+                gameObject.GetComponent<SphereCollider>().radius = 0.01f; 
+            }
         }
 
-        if (pose != HandPoses.NoPose) ShowPoints();
     }
 
     private void ShowPoints()
